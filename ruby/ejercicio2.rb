3056de9bae7d4969a3c404dfcd0d13e2c752cf76
Martiks38 <83650309+Martiks38@users.noreply.github.com>
2022-08-16 21:32:54 -0300

    Update ejercicio2.rb

--- ruby/ejercicio2.rb
+++ ruby/ejercicio2.rb
@@ -10,7 +10,7 @@
   end
 
   - Teniendo en cuenta que las canciones saben responder al mensaje titulo...
-    Esto significa que cada canción en el array del atributo canciones tiene un método getter que devuelve el título de la canción.
+    Esto significa que cada canción en el array del atributo canciones tiene un método getter, titulo, que devuelve el título de la canción.
   
   - Definí el método nombres_de_las_canciones que responda el nombre de las canciones del Disco.
     El método nombre_de_las_canciones debe recorrer todo el array del atributo @canciones y generar una lista, un nuevo array, con el nombre de la canción, su título. Para ello, usaremos el método map en cuyo bloque tendremos un parámetro llamado cancion dentro de ||.
